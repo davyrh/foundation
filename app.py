3e3fbf2e8b8e00599f23fd78e3a4b0101cda5b87
davyrh <104127167+davyrh@users.noreply.github.com>
2022-04-23 09:37:55 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from keras.models import load_model
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+st.title("Identification the Condition of Foundation of the House")
+st.header("Architecture Image Detection")
+st.text("Upload an image of any piece in  Foundation's house for Identification ")
+from img_classification import teachable_machine_classification            
+uploaded_file = st.file_uploader("Choose an image of Foundation  in the house ...", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Identifying the foundation..")
+    label = teachable_machine_classification(image, 'keras_model_foundation.h5')
+    if label == 0:
+        st.write(" The foundation is good ")
+                          
+    else:
+        st.write("The foundation is bad")
+        
